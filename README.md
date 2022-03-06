1b230492c5ec88b7d59b9fbeca34dc68fb38e185
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2022-03-06 15:37:02 -0500

    refactor to builder API for parsing CLI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rmb.rs
 
-The closest thing to just scribbling something down on a sticky note 
+Closest thing to scribbling stuff on a sticky note 
 
 Usage
 
@@ -8,17 +8,23 @@ Usage
 # List links
 rmbrs links 
 # Add a link
-rmbrs link https://aws.amazon.com/premiumsupport/knowledge-center/rds-mysql-high-replica-lag/
+rmbrs links add https://aws.amazon.com/premiumsupport/knowledge-center/rds-mysql-high-replica-lag/
+# Remove a link (index of link in list of links)
+rmbrs links rm 1 
 
 # List todo items
 rmbrs todos
 # Add a todo item
-rmbrs todo review https://github.com/balena-os/balena-supervisor/pull/1805
+rmbrs todos add "review https://github.com/balena-os/balena-supervisor/pull/1805"
+# Remove a todo (index of todo in list of todos)
+rmbrs todos rm 1 
 
 # List previously created timers 
 rmbrs timers 
 # Create a timer that tells you something in 5 days
-rmbrs timer 5d "roll out new cluster settings"
+rmbrs timers add "roll out new cluster settings" 5d
 # Create a timer with a timestamp
-rmbrs timer 25/12/2021 "wish team merry christmas"
+rmbrs timers add "wish team merry christmas" 25/12/2021 
+# Remove a timer (index of todo in list of timers)
+rmbrs timers rm 1 
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,10 @@
-use clap::{Parser, Subcommand};
+use clap::ArgMatches;
 
 use std::fs;
 use std::str::FromStr;
 
+mod cli;
 mod rmbrs;
-
-#[derive(Parser)]
-#[clap(about = "Command line tool that remembe.rs things", long_about = None)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Print everything you wanted to remember
-    List {},
-    /// Remove a link
-    RmLink {
-        /// Link index to remove
-        index: usize,
-    },
-    /// Add a URL
-    Link {
-        /// Link to remember
-        link: String,
-    },
-    /// List URLs
-    Links {},
-    /// Remove a todo
-    RmTodo {
-        /// Todo index to remove
-        index: usize,
-    },
-    /// Add a todo list item
-    Todo {
-        /// Todo item to remember
-        todo: String,
-    },
-    /// List Todo list items
-    Todos {},
-    /// Remove a timer
-    RmTimer {
-        /// Timer index to remove
-        index: usize,
-    },
-    /// Add a timer to remind you of something later
-    Timer {
-        /// What to remind
-        what: String,
-        /// When to remind
-        when: String,
-    },
-    /// List timers
-    Timers {},
-}
 
 fn main() {
     // TODO allow specifying data store location
@@ -68,9 +18,9 @@ fn main() {
         Err(_) => rmbrs::Remembers::new(),
     };
     // Parse CLI arguments provided
-    let args = Cli::parse();
+    let cmd = cli::build_cli().get_matches();
     // Check if something was changed
-    if handle_cmd(args.command, &mut remembered) {
+    if handle_cmd(cmd, &mut remembered) {
         // Persist change
         fs::write(data_path, remembered.to_string()).expect("Unable to write file")
     }
@@ -81,56 +31,102 @@ fn exit_msg(message: &str, code: i32) -> ! {
     std::process::exit(code);
 }
 
-fn handle_cmd(cmd: Commands, data: &mut rmbrs::Remembers) -> bool {
-    match cmd {
-        Commands::RmLink { index } => {
-            data.links.remove(index - 1);
-            println!("Removed Link");
-            true
-        }
-        Commands::Link { link } => {
-            data.links.add(link);
-            println!("Added Link");
-            true
-        }
-        Commands::RmTodo { index } => {
-            data.todos.remove(index - 1);
-            println!("Removed Todo");
-            true
-        }
-        Commands::Todo { todo } => {
-            data.todos.add(todo);
-            println!("Added Todo");
-            true
-        }
-        Commands::Timer { what, when } => {
-            println!("Will remind you to {what} in {when}");
-            data.timers.add(what, when);
-            true
-        }
-        Commands::RmTimer { index } => {
-            data.timers.remove(index - 1);
-            println!("Removed Timer"); 
-            true
-        }
-        Commands::List {} => {
+fn handle_cmd(cmd: ArgMatches, data: &mut rmbrs::Remembers) -> bool {
+    match cmd.subcommand() {
+        Some(sub_cmd) => {
+            handle_sub(&cli::SubCommand::from_str(sub_cmd.0).unwrap(), sub_cmd.1, data)
+        },
+        None => {
             println!(
                 "Remembers\n   Links\n{}\n   Todos\n{}\n   Timers\n{}",
                 data.links, data.todos, data.timers
             );
             false
+        } 
+    }
+}
+
+fn handle_sub(cmd: &cli::SubCommand, args: &ArgMatches, data: &mut rmbrs::Remembers) -> bool {
+    match (cmd, args.subcommand()) {
+        (cli::SubCommand::Links, sub_cmd) => {
+            match sub_cmd {
+                Some(more_sub_cmd) => {
+                    match (more_sub_cmd.0, more_sub_cmd.1) {
+                        ("add", aargs) => {
+                            data.links.add(aargs.value_of("link").unwrap().to_string());
+                            println!("Added link");
+                            true
+                        }
+                        ("rm", aargs) => {
+                            let index = aargs.value_of("index").unwrap();
+                            data.links.remove(index.parse::<usize>().unwrap() - 1);
+                            println!("Removed link");
+                            true
+                        }
+                        _ => {
+                            unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`");
+                        }
+                    }
+                },
+                None => {
+                    println!("{}", data.links);
+                    false
+                }
+            }
         }
-        Commands::Links {} => {
-            println!("{}", data.links);
-            false
+        (cli::SubCommand::Todos, sub_cmd) => {
+            match sub_cmd {
+                Some(more_sub_cmd) => {
+                    match (more_sub_cmd.0, more_sub_cmd.1) {
+                        ("add", aargs) => {
+                            data.todos.add(aargs.value_of("todo").unwrap().to_string());
+                            println!("Added todo");
+                            true
+                        }
+                        ("rm", aargs) => {
+                            let index = aargs.value_of("index").unwrap();
+                            data.todos.remove(index.parse::<usize>().unwrap() - 1);
+                            println!("Removed todo");
+                            true
+                        }
+                        _ => {
+                            unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`");
+                        }
+                    }
+                },
+                None => {
+                    println!("{}", data.todos);
+                    false
+                }
+            }
         }
-        Commands::Todos {} => {
-            println!("{}", data.todos);
-            false
-        }
-        Commands::Timers {} => {
-            println!("{}", data.timers);
-            false
+        (cli::SubCommand::Timers, sub_cmd) => {
+            match sub_cmd {
+                Some(more_sub_cmd) => {
+                    match (more_sub_cmd.0, more_sub_cmd.1) {
+                        ("add", aargs) => {
+                            let what = aargs.value_of("what").unwrap().to_string();
+                            let when = aargs.value_of("when").unwrap().to_string();
+                            data.timers.add(what, when);
+                            println!("Added timer");
+                            true
+                        }
+                        ("rm", aargs) => {
+                            let index = aargs.value_of("index").unwrap();
+                            data.timers.remove(index.parse::<usize>().unwrap() - 1);
+                            println!("Removed link");
+                            true
+                        }
+                        _ => {
+                            unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`");
+                        }
+                    }
+                },
+                None => {
+                    println!("{}", data.timers);
+                    false
+                }
+            }
         }
     }
 }
